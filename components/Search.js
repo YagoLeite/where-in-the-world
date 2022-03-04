0f5d75b09228fea1bc970570e38bb5a5acf3175d
Yago Batista <yagobatistafis@yahoo.com.br>
2022-03-04 20:32:06 -0300

    adding search component

---
+++ components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { InputGroup, Input, InputLeftElement } from "@chakra-ui/react";
+import { FiSearch } from "react-icons/fi";
+
+const Search = () => {
+  return (
+    <InputGroup w="96" display={{ base: "none", md: "flex" }}>
+      <InputLeftElement color="gray.500">
+        <FiSearch />
+      </InputLeftElement>
+      <Input placeholder="Search for articles..." />
+    </InputGroup>
+  );
+};
+
+export default Search;
