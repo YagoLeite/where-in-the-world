807d0a73fa410fffd23b030933066013a3a9da60
Yago Batista <yagobatistafis@yahoo.com.br>
2022-03-09 22:39:22 -0300

    adding leftdetail component

---
+++ components/details/LeftDetail.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { VStack, Text, Flex } from "@chakra-ui/react";
+
+const LeftDetail = (props) => {
+  return (
+    <VStack
+      alignItems="left"
+      justifyContent="center"
+      w="100%"
+      h="100%"
+      fontSize="14px"
+    >
+      <Text fontWeight={800} fontSize="18px">
+        {props.data?.name}
+      </Text>
+      <Flex gap={1}>
+        <Text fontWeight={600}>Native Name: </Text>
+        <Text fontWeight={300}>{props.data?.nativeName}</Text>
+      </Flex>
+      <Flex gap={1}>
+        <Text fontWeight={600}>Population: </Text>
+        <Text fontWeight={300}>{props.data?.population}</Text>
+      </Flex>
+      <Flex gap={1}>
+        <Text fontWeight={600}>Region: </Text>
+        <Text fontWeight={300}>{props.data?.region}</Text>
+      </Flex>
+      <Flex gap={1}>
+        <Text fontWeight={600}>Sub Region: </Text>
+        <Text fontWeight={300}>{props.data?.subregion}</Text>
+      </Flex>
+      <Flex gap={1}>
+        <Text fontWeight={600}>Capital: </Text>
+        <Text fontWeight={300}>{props.data?.capital}</Text>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default LeftDetail;
