5d369771fdcf895c6fdb7d9f5aca3c6d42042ec6
Yago Batista <yagobatistafis@yahoo.com.br>
2023-01-04 15:07:16 -0300

    change: header update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,98 +1,55 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
-  chakra,
   Flex,
-  HStack,
-  Icon,
   IconButton,
   Link,
   useColorMode,
   useColorModeValue,
-  Box,
-  Text,
+  Heading,
 } from "@chakra-ui/react";
-import { useViewportScroll } from "framer-motion";
-import { FaMoon, FaSun, FaHeart } from "react-icons/fa";
-import { AiFillGithub } from "react-icons/ai";
+
+import { BsMoon, BsSun } from "react-icons/bs";
+import { FiGithub } from "react-icons/fi";
 
 const Header = () => {
   const { toggleColorMode: toggleMode } = useColorMode();
   const text = useColorModeValue("dark", "light");
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
-
-  const bg = useColorModeValue("white", "gray.800");
-  const ref = React.useRef();
-  const [y, setY] = React.useState(0);
-  const { height = 0 } = ref.current ? ref.current.getBoundingClientRect() : {};
-
-  const { scrollY } = useViewportScroll();
-  useEffect(() => {
-    return scrollY.onChange(() => setY(scrollY.get()));
-  }, [scrollY]);
+  const SwitchIcon = useColorModeValue(BsMoon, BsSun);
 
   return (
-    <Box pos="relative">
-      <chakra.header
-        ref={ref}
-        shadow={y > height ? "sm" : undefined}
-        transition="box-shadow 0.2s"
-        bg={bg}
-        w="full"
-        overflowY="hidden"
-      >
-        <chakra.div h="4.5rem" mx="auto" maxW="1440px">
-          <Flex w="full" h="full" px="6" align="center" justify="space-between">
-            <Flex align="center" w="100%">
-              <Link href="/">
-                <Text
-                  w="100%"
-                  fontWeight={800}
-                  fontSize={["20px", "24px", "28px"]}
-                >
-                  Where in the world?
-                </Text>
-              </Link>
-            </Flex>
-
-            <Flex
-              justify="flex-end"
-              w="full"
-              maxW="824px"
-              align="center"
-              color="gray.400"
-              gap={1}
-            >
-              <HStack>
-                <Link
-                  isExternal
-                  aria-label="Go to Choc UI GitHub page"
-                  href="https://github.com/YagoLeite"
-                >
-                  <Icon
-                    as={AiFillGithub}
-                    display="block"
-                    transition="color 0.2s"
-                    w="5"
-                    h="5"
-                    _hover={{ color: "gray.600" }}
-                  />
-                </Link>
-              </HStack>
-              <IconButton
-                size="md"
-                fontSize="lg"
-                aria-label={`Switch to ${text} mode`}
-                variant="ghost"
-                color="current"
-                ml={{ base: "0", md: "3" }}
-                onClick={toggleMode}
-                icon={<SwitchIcon />}
-              />
-            </Flex>
-          </Flex>
-        </chakra.div>
-      </chakra.header>
-    </Box>
+    <Flex w="100%" justify="space-between" align="center">
+      <Heading h="100%" fontWeight={800} fontSize={["20px", "24px", "28px"]}>
+        Where in the world
+      </Heading>
+      <Flex h="100%" justify="center" align="center">
+        <Link
+          isExternal
+          aria-label="Yago Leite GitHub page"
+          href="https://github.com/YagoLeite"
+        >
+          <IconButton
+            size="md"
+            fontSize="lg"
+            aria-label={`Switch to ${text} mode`}
+            variant="ghost"
+            color="current"
+            onClick={toggleMode}
+            _hover={{ color: "gray.600" }}
+            icon={<FiGithub />}
+          />
+        </Link>
+        <IconButton
+          size="md"
+          fontSize="lg"
+          aria-label={`Switch to ${text} mode`}
+          variant="ghost"
+          color="current"
+          _hover={{ color: "gray.600" }}
+          onClick={toggleMode}
+          icon={<SwitchIcon />}
+        />
+      </Flex>
+    </Flex>
   );
 };
 
